13fe62fcaae6321d0ed1201d9491e3f92ea458ed
raidho-mx <miranda@raidho.mx>
2017-03-31 17:43:14 -0600

    Listado de Pendientes Stable

---
+++ inc/calendar.php
@@ -0,0 +1,122 @@
+<table>
+	<thead>
+		<tr>
+			<th class="week-end" scope="col"><span title="Sunday">D</span></th>
+			<th scope="col"><span title="Monday">L</span></th>
+			<th scope="col"><span title="Tuesday">M</span></th>
+			<th scope="col"><span title="Wednesday">M</span></th>
+			<th scope="col"><span title="Thursday">J</span></th>
+			<th scope="col"><span title="Friday">V</span></th>
+			<th class="week-end" scope="col"><span title="Saturday">S</span></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="ui-datepicker-week-end ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td>
+			<td class="ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td>
+			<td class="ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">1</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">2</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">3</a>
+			</td>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">4</a>
+			</td>
+		</tr>
+		<tr>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">5</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">6</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">7</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">8</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default activity" href="#">9</a>
+			</td>
+			<td class="activity" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">10</a>
+			</td>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">11</a>
+			</td>
+		</tr>
+		<tr>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">12</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">13</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">14</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">15</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">16</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">17</a>
+			</td>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">18</a>
+			</td>
+		</tr>
+		<tr>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">19</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">20</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">21</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">22</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">23</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">24</a>
+			</td>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">25</a>
+			</td>
+		</tr>
+		<tr>
+			<td class=" ui-datepicker-week-end" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">26</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">27</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">28</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">29</a>
+			</td>
+			<td class="" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default" href="#">30</a>
+			</td>
+			<td class=" ui-datepicker-days-cell-over ui-datepicker-today" data-event="click" data-handler="selectDay" data-month="2" data-year="2017">
+				<a class="ui-state-default ui-state-highlight" href="#">31</a>
+			</td>
+			<td class="ui-datepicker-week-end ui-datepicker-other-month ui-datepicker-unselectable ui-state-disabled">&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
---
+++ pendientes.php
@@ -0,0 +1,111 @@
+<?php
+
+	$headClass = 'blue';
+	include 'header.php'; ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="three columns sidebar filter">
+				<form class="" action="#" method="post">
+					<ul>
+						<li class="calendar">
+							<div class="title">
+								<a href="#" class="nav prev">&lt;</a>
+								<a href="#">Abril 2017</a>
+								<a href="#" class="nav next">&gt;</a>
+							</div>
+							<div class="month" style="display:none">
+								<?php include 'inc/calendar.php'; ?>
+							</div>
+						</li>
+						<li class="search">
+							<div>
+								<input type="text" placeholder="Busca un pendiente" name="" value="">
+								<button type="button" name="button"><img src="http://placehold.it/24" alt=""></button>
+							</div>
+						</li>
+						<li>
+							<div class="dropdown">
+								<select class="" name="">
+									<option value="">Todos los Usuarios</option>
+									<option value="">Martín Martinez</option>
+									<option value="">Gonzalo Gonzalez</option>
+									<option value="">Domingo Domínguez</option>
+									<option value="">Ana Ancira</option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<div class="dropdown">
+								<select class="" name="">
+									<option value="">Etiqueta</option>
+									<option value="">Productividad</option>
+									<option value="">Administración</option>
+									<option value="">MinuteApp</option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<div class="dropdown">
+								<select class="" name="">
+									<option value="">Prioridad</option>
+									<option value="">Alta</option>
+									<option value="">Media</option>
+									<option value="">Baja</option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<div class="dropdown">
+								<select class="" name="">
+									<option value="">Junta</option>
+									<option value="">Productividad</option>
+									<option value="">Administración</option>
+									<option value="">MinuteApp</option>
+								</select>
+							</div>
+						</li>
+						<li>
+							<input type="radio" name="showdone" value="showdone"> <label for="showdone">Mostrar Resueltos</label>
+						</li>
+					</ul>
+				</form>
+				<div class="actions">
+					<p>Filtrando...</p>
+				</div>
+			</div>
+
+
+			<div class="nine columns listings">
+				<h2>Pendientes para <strong>Abril 5</strong></h2>
+
+				<div class="cards"><?php
+					echo card('long overdue');
+					echo card('long single');
+					echo card('long done'); ?>
+				</div>
+
+				<h2>Pendientes para <strong>Abril 9</strong></h2>
+
+				<div class="cards"><?php
+					echo card('long single');
+					echo card('long '); ?>
+				</div>
+
+				<h2>Pendientes para <strong>Abril 10</strong></h2>
+
+				<div class="cards"><?php
+					echo card('long single');
+					echo card('long single');
+					echo card('long '); ?>
+				</div>
+
+				<div class="loadcard">
+					<p>Filtrando...</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
