f618a566792827f8aa61df0a9cd440e353264ca5
raidho-mx <miranda@raidho.mx>
2017-03-30 20:15:54 -0600

    Login stable

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Minute</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,700" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/index.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body class="login">
+
+	<div class="modal">
+		<div class="head">
+			<a href="#" class="logo"><img src="http://placehold.it/170x40" alt=""></a>
+		</div>
+		<div class="content">
+			<p>Inicia sesión con tus redes:</p>
+			<div class="social">
+				<a href="#" class="circle button"><img src="http://placehold.it/20" alt=""></a>
+				<a href="#" class="circle button"><img src="http://placehold.it/20" alt=""></a>
+				<a href="#" class="circle button"><img src="http://placehold.it/20" alt=""></a>
+			</div>
+			<div class="manual">
+				<p>o con tus datos:</p>
+				<a href="#" class="action float_right">Registrar</a>
+				<form class="" action="" method="post">
+					<input type="email" name="" value="" placeholder="Correo">
+					<input type="password" name="" value="" placeholder="Contraseña">
+					<div class="alert">Correo o contraseña invalida.</div>
+					<a href="#" class="small">¿Olvidaste tu contraseña?</a>
+					<button type="submit" name="button" class="squared button teal">Entrar</button>
+				</form>
+
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
