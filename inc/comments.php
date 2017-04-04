5fba1174251f6f5731f9c9a6004e3d233a6ce8a8
raidho-mx <miranda@raidho.mx>
2017-04-03 19:28:46 -0500

    Comments included externally

---
+++ inc/comments.php
@@ -0,0 +1,60 @@
+<div class="drawer right">
+	<div class="wrap">
+		<h3>Comentarios (<span class="amount">2</span>)</h3>
+	</div>
+	<div class="comments wrap">
+		<div class="blank">
+			No hay comentarios aún.
+		</div>
+
+		<div class="incomming message">
+			<div class="profile picture">
+				<img src="http://placehold.it/42" alt="Nombre Apellido">
+			</div>
+			<div class="data">
+				<div class="name info">
+					Nombre Apellido <span>27 febrero, a las 4:00 pm</span>
+				</div>
+				<div class="content">
+					¿Si logramos salir con prototipo para el viernes 10? <a href="#">http://google.com</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="outgoing message">
+			<div class="profile picture">
+				<img src="http://placehold.it/42" alt="Nombre Apellido">
+			</div>
+			<div class="data">
+				<div class="name info">
+					Nombre Apellido <span>27 febrero, a las 4:00 pm</span>
+				</div>
+				<div class="content">
+					¿Si logramos salir con prototipo para el viernes 10? <a href="#">http://google.com</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="incomming message">
+			<div class="profile picture">
+				<img src="http://placehold.it/42" alt="Nombre Apellido">
+			</div>
+			<div class="data">
+				<div class="name info">
+					Nombre Apellido <span>27 febrero, a las 4:00 pm</span>
+				</div>
+				<div class="content">
+					¿Si logramos salir con prototipo para el viernes 10? <a href="#">http://google.com</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="comment reply">
+		<form class="wrap" action="index.html" method="post">
+			<input type="text" placeholder="Escribe un comentario..." name="" value="">
+			<input class="text-button blue" type="submit" name="" value="Enviar">
+		</form>
+	</div>
+</div>
