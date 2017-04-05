ddb89e4b681a0dab0c18c4b75b5767f932e244bd
raidho-mx <miranda@raidho.mx>
2017-04-04 22:00:02 -0500

    Stats module

---
+++ inc/stats.php
@@ -0,0 +1,21 @@
+
+<div class="stats">
+	<div class="stat">
+		<div class="number">
+			34
+		</div>
+		<h4>GENERAL</h4>
+	</div>
+	<div class="stat">
+		<div class="number">
+			14
+		</div>
+		<h4>RESUELTOS</h4>
+	</div>
+	<div class="stat">
+		<div class="number">
+			24
+		</div>
+		<h4>ATRASOS</h4>
+	</div>
+</div>
