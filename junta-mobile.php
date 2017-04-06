9ee11931d040e3fa1faeebc021ea3f6f7d8ab6f5
raidho-mx <miranda@raidho.mx>
2017-04-06 13:05:20 -0500

    Meeting mobile stable

--- junta-mobile.php
+++ junta-mobile.php
@@ -1,30 +1,31 @@
 <?php
 
-	$headClass = 'blue';
-	$bodyClass = 'task detail-view mobile';
+	$headClass = 'yellow';
+	$bodyClass = 'meeting detail-view mobile';
 	include 'header.php'; ?>
 
 
 	<div class="drawer left">
 		<div class="wrap">
-			<div class="task title">
-				<div class="circle profiles">
-					<div>
-						<img src="http://placehold.it/42" alt="">
-					</div>
-					<div>
-						<img src="http://placehold.it/42" alt="">
-					</div>
+			<h1>Junta de Arranque Proyecto Minute App</h1>
+			<div class="info">
+				<p class="tag date"><strong>27 febrero</strong>, a las <strong>4:00 pm</strong></p>
+				<p class="via"><img src="img/ic-place-yellow.svg" alt=""> <strong>Via Skype</strong></p>
+				<div class="attending profiles">
+					<h3>Participantes</h3>
+					<a href="#" data-tooltip-content="Nombre Apellido">
+						<img src="http://placehold.it/42" alt="Nombre Apellido">
+						<h4>Nombre</h4>
+					</a>
+					<a href="#" data-tooltip-content="Nombre Apellido">
+						<img src="http://placehold.it/42" alt="Nombre Apellido">
+						<h4>Nombre</h4>
+					</a>
+					<a href="#" class="add" data-tooltip-content="Nombre Apellido">
+						<img src="http://placehold.it/42/FFFFFF/003366" alt="Nombre Apellido">
+						<h4>Agregar</h4>
+					</a>
 				</div>
-				<h1>Definición Inicial Minute</h1>
-				<p>Responsables: <strong>Pedro López</strong></p>
-			</div>
-			<div class="task info">
-				<p class="priority-icon med"><strong>Prioridad media</strong></p>
-				<p>Entrega: <strong class="tag blue">27 febrero</strong></p>
-				<p>Etiquetas: <strong class="tag">Skype</strong> <strong class="tag">MinuteApp</strong></p>
-				<p class="ellipsis">Origen: <a href="junta-mobile.php"><strong class="tag yellow">Junta de Arranque Proyecto Minute App</strong></a></p>
-				<hr>
 			</div>
 		</div>
 	</div>
@@ -40,8 +41,10 @@
 		</div>
 	</div>
 
-	<h3 class="tab">Tareas (<span class="amount">5</span>)</h3>
+
+	<h3 class="tab">Pendientes (<span class="amount">5</span>)</h3>
 	<div class="middle tasks under">
+		<h3>Pendientes (<span class="amount">2</span>)</h3>
 		<div class="cards"><?php
 			echo card('square overdue');
 			echo card('square single');
@@ -79,10 +82,12 @@
 		<?php include 'inc/comments.php'; ?>
 	</div>
 
+
 	<div class="two actions">
 		<a href="#" class="text-button left edit">Editar</a>
-		<a href="#" class="button blue resolve">Resolver <div class="checkbox"></div></a>
+		<!-- <a href="#" class="button blue resolve">Resolver <div class="checkbox"></div></a> -->
 	</div>
+
 
 </body>
 </html>
