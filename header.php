dd6abefa9e70ec1888518c05de2378d0f79376d9
Daniel Miranda <mirand@raidho.mx>
2017-04-06 19:00:11 -0500

    Fully Responsive Header

--- header.php
+++ header.php
@@ -2,32 +2,40 @@
 <html lang="en">
 <head>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Minute</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+	<!-- Basic Page Needs
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta charset="utf-8">
+	<title>Minute</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Mobile Specific Metas
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet" type="text/css">
+	<!-- FONT
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet" type="text/css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/index.css">
+	<!-- CSS
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/index.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script>
+	$(document).ready(function(){
+		$('a.menu-toggle').click(function(){
+			$('header').toggleClass('open-menu');
+		});
+	});
+	</script>
 
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
+	<!-- Favicon
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
 
-  <?php include 'functions.php'; ?>
+	<?php include 'functions.php'; ?>
 </head>
 <body class="<?php echo $bodyClass; ?>">
 	<header class="<?php
@@ -38,13 +46,19 @@
 		}
 	?>">
 		<div class="container">
-			<a href="#" class="logo"><img src="<?php
-			if(strpos($headClass, 'yellow') !== false) {
-				echo 'img/logo-black.svg';
-			} else {
-				echo 'img/logo-white.svg';
-			}
-			?>" alt=""></a>
+			<a href="#" class="mobile-button back"><img src="img/ic-arr-white.svg" alt="Back"></a>
+			<a href="#" class="logo">
+				<img src="<?php
+				if(strpos($headClass, 'yellow') !== false) {
+					echo 'img/logo-black.svg';
+				} else {
+					echo 'img/logo-white.svg';
+				}
+				?>" alt="">
+			</a>
+			<div class="mobile breadcrumbs">
+				<p class="ellipsis">Nombre de Pendiente, Todos los pendientes, de Donde vienes, etc...</p>
+			</div>
 			<nav>
 				<a href="index.php" class="current">Inicio</a>
 				<a href="pendientes.php">Pendientes</a>
@@ -63,5 +77,6 @@
 					<span class="alert">1</span>
 				</a>
 			</nav>
+			<a href="#" class="mobile-button menu-toggle"><div class="has-alert"></div><hr></a>
 		</div>
 	</header>
