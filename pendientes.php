64191caddcf99aa5b3ea7334273ee698410505eb
Daniel Miranda <mirand@raidho.mx>
2017-04-06 20:10:49 -0500

    Listings: responsive cards

--- pendientes.php
+++ pendientes.php
@@ -5,7 +5,7 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="three columns sidebar filter">
+			<div class="three columns sidebar filter" style="display:none">
 				<form class="" action="#" method="post">
 					<ul>
 						<li class="calendar">
